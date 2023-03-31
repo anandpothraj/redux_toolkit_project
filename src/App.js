a31e0d0b11cd85f90f10fb143a92c757c8dde3c3
anandpothraj <anandpothraj11052001@gmail.com>
2023-03-31 23:15:22 +0530

    Completed React redux toolkt

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
 import './App.css';
 import CakeView from './features/cake/CakeView';
-// import UserView from './features/user/UserView';
+import { UserView } from './features/user/UserView';
 import IcecreamView from './features/icecream/IcecreamView';
 
 const App = () => {
   return (
-    <div style={{margin : "35vh auto", maxWidth:"50vw"}}>
+    <div style={{margin : "10vh auto", maxWidth:"50vw"}}>
       <CakeView/>
       <IcecreamView/>
-      {/* <UserView/> */}
+      <UserView/>
     </div>
   );
 };
--- src/features/user/UserView.jsx
+++ src/features/user/UserView.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { fetchUsers } from './userSlice';
+import { useSelector, useDispatch } from 'react-redux';
 
-const UserView = () => {
+export const UserView = () => {
+  
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  },[]);
+  
   return (
-    <div>UserView</div>
+    <div>
+      <h2>List of users</h2>
+      {user.loading && <div>Loading...</div>}
+      {!user.loading && user.error ? <div>Error : {user.error}</div> : null}
+      {
+        !user.loading && user.users.length > 0 ? (
+          <ul>
+            {user.users.map(user => (
+              <li key={user.id}>{user.name}</li>
+            ))}
+          </ul>
+        ) : null
+      }
+    </div>
   )
 }
-
-export default UserView
